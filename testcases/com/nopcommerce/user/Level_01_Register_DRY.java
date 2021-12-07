efe95b10688903ce4a8b22d5b18f736969787704
Huynh Minh Hiep <hmhiep93@gmail.com>
2021-12-08 01:19:29 +0700

    Updated testcases User

--- testcases/com/nopcommerce/user/Level_01_Register_DRY.java
+++ testcases/com/nopcommerce/user/Level_01_Register_DRY.java
@@ -12,7 +12,7 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 
-public class User_01_Register {
+public class Level_01_Register_DRY {
 	WebDriver driver;
 	String emailAddress;
 	String projectPath = System.getProperty("user.dir");
